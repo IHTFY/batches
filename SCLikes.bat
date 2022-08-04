4d43964cb16291ab7511b9b57e8e2fbc7802216f
Frankie Mercado <oeluer@gmail.com>
2022-08-04 03:28:49 -0700

    download all soundcloud likes

---
+++ SCLikes.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+SET uid=IHTFY
+SET dest=C:\Users\Frankie\Music\%uid% soundcloud likes\
+ECHO saving in %dest%
+SET ytStr="%dest%%%(uploader)s - %%(title)s (%%(id)s).%%(ext)s"
+SET archStr="%dest%/archive"
+ECHO %ytStr%
+START yt-dlp --download-archive %archStr% -ciw https://soundcloud.com/%uid%/likes -o %ytStr%
