a241ebe59171e123d0d3c28fc77d1dd7229df60a
Frankie Mercado <oeluer@gmail.com>
2022-11-21 11:28:09 -0800

    better frame extraction

--- folder2frames.bat
+++ folder2frames.bat
@@ -1,2 +1,2 @@
 REM split all videos into frames. Must make a frames/ directory
-for %%A in ("*.mp4") do ffmpeg -i "%%A" -q:v 1 -qmin 1 "frames\%%~nA_out-%%06d.jpg"
+for %%A in ("*.mp4") do ffmpeg -i "%%A" -q:v 2 "frames\%%~nA_out-%%06d.jpg"
